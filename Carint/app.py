a721df1ae3a57ae7ad0cc9783eebadb56079bba7
Damla Gül <damlasaykara@gmail.com>
2023-12-24 08:58:42 +0300

    admin paneli user

--- Carint/app.py
+++ Carint/app.py
@@ -5,6 +5,7 @@ from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = 'mysecret'
 db = SQLAlchemy(app)
 admin = Admin(app)
 
@@ -12,6 +13,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 admin.add_view(ModelView(User, db.session)) 
 
