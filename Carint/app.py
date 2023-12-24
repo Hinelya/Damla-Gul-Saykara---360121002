145513b55410032e87f21008415680329987fabc
Damla Gül <damlasaykara@gmail.com>
2023-12-24 08:44:49 +0300

    admin panel 1

--- Carint/app.py
+++ Carint/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_admin import Admin
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+admin = Admin(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+admin.add_view(ModelView(User, db.session)) 
 
 @app.route('/')
 def index():
